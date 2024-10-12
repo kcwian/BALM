d7a57b0651b5046120d8c82c15ca1a072d6b9504
Krzysztof Cwian <krzysztof.cwian@gmail.com>
2024-10-12 13:13:11 +0000

    Update of launch files

--- calculate_balm_nc.sh
+++ calculate_balm_nc.sh
@@ -4,6 +4,8 @@ for i in $(seq 1 1)
 do
   roslaunch balm2 stairs.launch voxel_size:=$i
 done
+roslaunch balm2 stairs.launch voxel_size:=0.2
+
 for i in $(seq 1 1)
 do
   roslaunch balm2 cloister.launch voxel_size:=$i
